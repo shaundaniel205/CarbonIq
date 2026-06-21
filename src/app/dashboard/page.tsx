648285d81ef01391e3aa53ff71ded2a14f1a1bf0
Shaun Dani <shaundaniel600@gmail.com>
2026-06-22 00:06:06 +0300

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+export const dynamic = 'force-dynamic';
 import React, { useState, useEffect, useCallback } from 'react'
 import Link from 'next/link'
 import Navigation from '@/components/navigation/navigation'
